2fc3abb9bf3f1cd4beedd56d1f34408ea947a9d3
Diego jimenes <lligaskate@gmail.com>
2019-02-01 16:13:37 -0200

    initial commit1683

--- config/database.js
+++ config/database.js
@@ -18,7 +18,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get("DB_CONNECTION", "pg"),
+  connection: Env.get("DB_CONNECTION", Env.get("DB_CONNECTION")),
 
   /*
   |--------------------------------------------------------------------------
@@ -57,8 +57,8 @@ module.exports = {
     connection: {
       host: Env.get("DB_HOST", CLEARDB_DATABASE_URL.host),
       port: Env.get("DB_PORT", ""),
-      user: Env.get("DB_USER", CLEARDB_DATABASE_URL.username),
-      password: Env.get("DB_PASSWORD", CLEARDB_DATABASE_URL.password),
+      user: Env.get("DB_USER", CLEARDB_DATABASE_URL.user),
+      password: Env.get("DB_PASSWORD", CLEARDB_DATABASE_URL.pass),
       database: Env.get("DB_DATABASE", CLEARDB_DATABASE_URL.pathname.substr(1))
     },
     debug: Env.get("DB_DEBUG", false)
