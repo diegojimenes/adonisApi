1801f631c654f7bbdc16f79765e2a2e6d38df0be
Diego jimenes <lligaskate@gmail.com>
2019-02-01 14:53:40 -0200

    initial commit4

--- config/app.js
+++ config/app.js
@@ -26,7 +26,7 @@ module.exports = {
   | to encrypt cookies, sessions and other sensitive data.
   |
   */
-  appKey: Env.getOrFail("Lu5BaT5Au4D3CNfp43olfPliiveZL4qL"),
+  appKey: Env.getOrFail("APP_KEY"),
 
   http: {
     /*
